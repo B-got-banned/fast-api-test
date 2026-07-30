b27480e79df93ec32cebd24ebf45f93426859f06
Bethel Onyealilachi <onyealilachib0850@student.babcock.edu.ng>
2026-07-30 01:10:11 +0100

    Basic CRUD API for posts

---
+++ main.py
@@ -0,0 +1,67 @@
+from typing import Optional
+from fastapi import FastAPI, Request, Response, status, HTTPException
+from fastapi import Body
+from pydantic import BaseModel
+
+app = FastAPI()
+#Post Model
+class Post(BaseModel):
+    post_id: Optional[int] = None
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+#List for dummy data posts  
+posts: list[Post] = []
+
+#Helper function to find post by id
+def find_post(id):
+    for p in posts:
+        if id == p.post_id:
+            return p
+
+#Root route
+@app.get('/')
+def root():
+    return {"message": "Konnichiwa! :D"}
+
+#Create a post
+@app.post('/posts', status_code=status.HTTP_201_CREATED)
+def create_post(post: Post):
+    if post.post_id == None:
+        post.post_id = len(posts) + 1
+    posts.append(post)
+    return {"data": post}
+
+#Get all posts
+@app.get('/posts')
+def get_posts():
+    return {"posts": posts}
+
+#Get post by id
+@app.get('/posts/{post_id}')
+def get_post(post_id: int):
+    post = find_post(post_id)
+    if post:
+        return {"message": f'Post with id {post_id} found!', "post": post}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {post_id} could not be found :(")
+
+#Delete a post
+@app.delete('/posts/{post_id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(post_id: int):
+    post = find_post(post_id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {post_id} does not exist :(")
+    posts.remove(post)
+
+#Update a post
+@app.put('/posts/{post_id}')
+def update_post(post_id: int, updated_post: Post):
+    post = find_post(post_id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {post_id} does not exist :(")
+    updated_post.post_id = post_id
+    posts.insert(posts.index(post), updated_post)
+    posts.remove(post)
+    return {"message": f"Post with id {post_id} successfully updated!", "data": updated_post}
